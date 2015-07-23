59a2d6d674eb752a75661999e202eb07662e69d1
MaBr101010 <Braun@braun.hecom.local>
2015-07-23 14:33:11 +0200

    templates & Models eingefügt

---
+++ schluesselverwaltung/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'Braun'
+
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    url(r'^all/$', 'schluesselverwaltung.views.mitarbeiter_all'),
+    url(r'^get/(?P<persoNr>\d+)/$', 'schluesselverwaltung.views.mitarbeiter'),
+    )
